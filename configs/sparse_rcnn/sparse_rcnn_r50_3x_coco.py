4182d0a6e2cf0c2b8efe2b37cddbb32e232c330b
tianyuandu <tianyuandu@gmail.com>
2020-12-04 12:35:02 +0800

    align evaluation mAP with official repo

--- configs/sparse_rcnn/sparse_rcnn_r50_3x_coco.py
+++ configs/sparse_rcnn/sparse_rcnn_r50_3x_coco.py
@@ -28,7 +28,7 @@ model = dict(
         proposal_feature_channel=256,
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=2),
             out_channels=256,
             featmap_strides=[4, 8, 16, 32]),
         bbox_head=dict(
